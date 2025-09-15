e9488da415cedc57154cb8e89dc142f37b78afd3
Ahmed Mustafa <ahmedd.mustafa.0@gmail.com>
2025-09-15 22:11:42 +0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,20 @@
+import requests
+
+api_key = "9a92f73bcd52b10554476b33bcbc0308"
+
+user_input = input("Enter a city name: ")
+
+weather_data = requests.get(f"https://api.openweathermap.org/data/2.5/weather?q={user_input}&appid={api_key}")
+
+if weather_data.json()["cod"] == "404":
+    print("no city found")
+else:
+
+    weather = weather_data.json()["weather"][0]["main"]
+    temp = round(weather_data.json()["main"]["temp"])
+
+    print(f"The weather in {user_input} is: {weather}")
+   
+    temp_celcius = temp - 273.15
+    print(f"The temperature in {user_input} is: {temp_celcius}°C")
+
